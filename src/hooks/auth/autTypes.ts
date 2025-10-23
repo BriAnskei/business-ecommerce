14167b8db754e95abe6d9042a58f906729af4e7c
Brian <brianebrahim7@gmail.com>
2025-10-23 11:31:22 +0800

    done with provider, working on auth for backend

--- src/hooks/auth/autTypes.ts
+++
@@ -1,9 +0,0 @@
-export interface SignUpFormInput {
-  name: string;
-  email: string;
-  password: string;
-  rememberMe: boolean;
-}
-
-// signin doesnt required nam, it requires remembeMe option
-export interface SignInFormInput extends Omit<SignUpFormInput, "name"> {}
